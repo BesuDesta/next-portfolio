d2e2cbba9cc536f50a59605e71e296694f470444
Nana Kofi Okae <89530363+Nan3rOkae@users.noreply.github.com>
2025-01-21 17:17:06 -0500

    temp removal of darkmode / analytics enabled

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Link from "next/link";
 import React from "react";
 
@@ -9,15 +10,17 @@ export default function Footer() {
           <p className="text-sm text-muted-foreground">Last updated on</p>
           <span className="current-date text-xs">January 21 2025</span>
         </div>
-        <p className=" flex gap-1 text-muted-foreground text-sm">
-          Made by
-          <Link
-            href="https://www.linkedin.com/in/nana-kofi-okae"
-            className="text-neutral-800 hover:underline "
-          >
-            Nana
-          </Link>
-        </p>
+        <div>
+          <p className=" flex gap-1 text-muted-foreground text-sm">
+            Made by
+            <Link
+              href="https://www.linkedin.com/in/nana-kofi-okae"
+              className="text-neutral-800 hover:underline "
+            >
+              Nana
+            </Link>
+          </p>
+        </div>
       </div>
     </footer>
   );
