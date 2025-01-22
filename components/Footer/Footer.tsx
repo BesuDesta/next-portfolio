dc0f669da545518404e53aa0394699d616d8d819
Nana Kofi Okae <89530363+Nan3rOkae@users.noreply.github.com>
2025-01-22 16:06:45 -0500

    added github repo link and fixed current date text size

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Github } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
@@ -10,7 +11,15 @@ export default function Footer() {
           <p className="text-sm text-muted-foreground">Last updated on</p>
           <span className="current-date text-xs">January 22 2025</span>
         </div>
-        <div>
+        <div className="flex gap-3 items-center">
+          <Link
+            href="https://github.com/Nan3rC0re/next-portfolio-template"
+            target="_blank"
+            className="bg-neutral-800 text-white text-sm flex gap-1 px-3 py-1 hover:bg-neutral-700 transition-colors duration-200 rounded-full items-center"
+          >
+            <Github size={12} />
+            Github repo
+          </Link>
           <p className=" flex gap-1 text-muted-foreground text-sm">
             Made by
             <Link
