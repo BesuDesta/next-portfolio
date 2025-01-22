7712f8a8110c67a6e3fbb20892aae2b64d175f15
Nana Kofi Okae <89530363+Nan3rOkae@users.noreply.github.com>
2025-01-22 13:15:22 -0500

    fixed some things and added icons for pop of color to projects section

--- components/Education/Education.tsx
+++ components/Education/Education.tsx
@@ -4,9 +4,9 @@ export default function Education() {
   return (
     <div className="section">
       <p className="section-header">Education</p>
-      <div className="flex flex-col">
+      <div className="flex flex-col gap-1">
         <p className="text-title">Virginia Commonwealth University</p>
-        <div className="flex flex-col gap-2 md:flex-row justify-between">
+        <div className="flex flex-col gap-1  md:flex-row justify-between">
           <p className="italic date-text">
             Bachelor of Science in Computer Science
           </p>
--- components/Experience/data.ts
+++ components/Experience/data.ts
@@ -33,12 +33,4 @@ export const experiences: Experience[] = [
     description:
       " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
-  {
-    title: "VIP Researcher",
-    company: "Acme",
-    logo: "/company-logos/logo-1.png",
-    date: "May 2023 - Aug 2023",
-    description:
-      " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.",
-  },
 ];
